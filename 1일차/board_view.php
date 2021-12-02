8aef850f863e563889c9fd1b84f0fad3667b0b8e
sieun park <89372116+singny@users.noreply.github.com>
2021-12-02 18:02:10 +0900

    Create board_view.php

---
+++ 1일차/board_view.php
@@ -0,0 +1,85 @@
+<?php
+$id = "separk2111";
+$user_id =  $id."님 반갑습니다.";
+?>
+<!DOCTYPE html>
+<html>
+<title>리뷰게시판</title>
+<style>
+  td, th {border:solid 1px}
+  table {border-collapse: collapse;}
+  body { background-image: url('./assets/8.PNG');}
+    body { background-size : cover;}  
+    header { background-color:lightsalmon;}
+    table { background-color: oldlace; }
+
+  ul { background-color: #FFDAB9; width: 300px; list-style-type: none; margin: 0; padding: 0; }
+  
+  li a { display: block; color: #000000; padding: 8px; text-decoration: none; font-weight: bold; }
+
+  li a:hover { background-color: #CD853F; color: white; }
+  li a.current { background-color: #FF6347; color: white; }
+
+  li a:hover:not(.current) { background-color: #CD853F; color: white;}
+  li { border-bottom: solid 1px black; }
+  
+  li:last-child { border-bottom: none; }
+
+
+
+</style>
+<body>
+<header>
+  <center>
+  <br>
+<h1>신입사원 게시판</h1>
+
+</center>
+<h3 align="right"><?=$user_id?></h3>
+</header>
+<br>
+
+<nav>
+  <center>
+  
+  <ul>
+
+    <!-- <li style="text-align:center" ><a href="main.html">메인페이지</a></li> -->
+
+
+
+</ul>
+<br>
+</center>
+</nav>
+
+<table style="width:100%">
+
+  <tr>
+ 
+    <th>제목</th>
+    <th>작성자</th>
+    <th>작성일</th>
+    <!-- <th>별점</th>
+    <th>사진</th> -->
+  </tr>
+  <tbody id="rows">
+    
+    <!-- <tr>
+ 
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> -->
+      
+</tbody>
+</table>
+<div style="width:100%; text-align: right;">
+  <br>
+  <button onclick="moveForm()">글작성</button>
+</div>
+
+</body>
+</html>
