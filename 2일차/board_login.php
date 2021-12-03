92feffc4ad770c751fecc2cd1f1116559cc74890
sieun park <89372116+singny@users.noreply.github.com>
2021-12-03 17:49:32 +0900

    Create board_login.php

---
+++ 2일차/board_login.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+    <form method="GET" action="board_login_process.php">
+        아이디 :  <input type="text" id="id" name="id" /> <br>
+        비밀번호 :  <input type="password" id="password" name="password" /><br><br>
+        <input type="submit" />
+        
+    </form>
+    <form method="GET" action="board_join.html">
+      <input type="submit" value="회원가입" />
+    </form>
+</body>
+</html>
