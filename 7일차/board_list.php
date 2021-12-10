84e7be4df55f449adc506302b3cd873929cd4e55
sieun park <89372116+singny@users.noreply.github.com>
2021-12-10 17:47:27 +0900

    Update board_list.php

--- 7일차/board_list.php
+++ 7일차/board_list.php
@@ -3,7 +3,56 @@
 //@error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
 if(!defined("_INCLUDE_")) require_once $_SERVER["DOCUMENT_ROOT"] . "/lib/include.php";
 @error_reporting(E_ALL);
-?><!DOCTYPE html>
+require_once  _LIB_PATH_ . "nav_page.class.php";
+$nav = new NavPage;
+$nav->start(5,10);
+?>
+<?php
+$db = new DB;
+//$db->Debug = true;
+$main_sql = "SELECT b.con_datetime, b.con_title, u.user_name, de.dept_name, b.con_vc , du.duty_name
+            FROM ex_user_set u, ex_dept_set de, board_contents b, ex_duty_set du
+            WHERE b.wr_user = u.user_id and b.wr_dept = de.dept_no and b.wr_duty = du.duty_no";
+$sql = "WITH A as (
+    {$main_sql}
+)
+SELECT COUNT(*) AS CNT FROM A";
+$count = $db->query_one($sql);
+$str_page_bar = $nav->navpage($count);
+$sql = $main_sql;
+$db->query_limit($sql, $nav->start_row, $nav->row_scale);
+$query_row_count = $db->nf();
+
+$str_data_row = null;
+if($query_row_count > 0)
+{
+    $i = 0;
+    while($db->next_record())
+    {
+        $row = $db->Record;
+        $no = $count - $nav->start_row - $i;
+        $date = substr($row["con_datetime"],0,10);
+        $dept_user = "[".$row["dept_name"]."] ".$row["user_name"]." ".$row["duty_name"];
+        $str_data_row .= "              
+        <tr>
+            <td style=\"text-align: center;\">{$no}</td>
+            <td style=\"text-align: center;\">{$date}</a></td>
+            <td>{$row["con_title"]}</td>
+            <td>{$dept_user}</td>
+            <td style=\"text-align: center;\">{$row["con_vc"]}</td>
+        </tr>
+           "  ;
+    $i++;
+    }
+}else {
+    $str_data_row = '      
+    <tr>
+                <td style="text-align: center;" colspan="5">직원 정보를 찾을수가 없습니다.</td>
+            </tr>
+          ';
+}
+?>
+<!DOCTYPE html>
 <!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="ko"> <![endif]-->
 <!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="ko"> <![endif]-->
 <!--[if IE 8]> <html class="lt-ie9" lang="ko"> <![endif]--> 
@@ -54,43 +103,16 @@ if(!defined("_INCLUDE_")) require_once $_SERVER["DOCUMENT_ROOT"] . "/lib/include
             <thead>
             <tr>
                 <th scope="cols" style="width: 25px; text-align :center;">NO.</th>
-                <th scope="cols" style="width: 200px; text-align :center;">작성일</th>
+                <th scope="cols" style="width: 250px; text-align :center;">작성일</th>
                 <th scope="cols" style="width: 650px;">TITLE</th>
                 <th scope="cols" style="width: 300px;">작성자</th>
                 <th scope="cols" style="width: 75px; text-align :center;">조회수</th>
             </tr>
             </thead>
             <tbody>
-<?php
-$db = new DB;
-//$db->Debug = true;
-$sql = "SELECT b.con_datetime, b.con_title, u.user_name, de.dept_name, b.con_vc , du.duty_name
-        FROM ex_user_set u, ex_dept_set de, board_contents b, ex_duty_set du
-        WHERE b.wr_user = u.user_id and b.wr_dept = de.dept_no and b.wr_duty = du.duty_no";
-$db->query($sql);
-$count = $db->nf();
-if($count > 0)
-{
-    $i = 0;
-    while($db->next_record())
-    {
-        $row = $db->Record;
-        $date = substr($row["con_datetime"],0,10);
-        $dept_user = "[".$row["dept_name"]."] ".$row["user_name"]." ".$row["duty_name"];
-?>
-                <tr>
-                    <td style="text-align :center"><?php echo $i + 1; ?></td>
-                    <td style="text-align :center"><?php echo $date;?></td>
-                    <td><?php echo $row["con_title"]; ?></td>
-                    <td><?php echo $dept_user; ?></td>
-                    <td style="text-align :center"><?php echo $row["con_vc"]; ?></td>
-                </tr>
-<?php
-        $i++;
-    }
-}
-?>
+                <?php echo $str_data_row;?>  
             </tbody>
         </table>
+        <?php echo $str_page_bar;?>
     </body>
 </html>
