e978623942d94922bcf7d5c5fadb6e8315bb70ed
sieun park <89372116+singny@users.noreply.github.com>
2021-12-20 17:31:03 +0900

    Update board_id_chk.php

--- 10일차/board_id_chk.php
+++ 10일차/board_id_chk.php
@@ -10,11 +10,35 @@ $db = new DB;
 $sql = "SELECT user_id FROM EX_USER_SET WHERE user_id = '{$post['user_id']}'";
 $db->query($sql);
 $count = $db->nf();
+
+$chk = null;
 if ($count == "0") {
-    echo "사용가능한 아이디 입니다.";
-}else if($count == "1") {
-    echo "중복된 아이디 입니다.";
-} else {
-    echo "아이디를 입력하세요.";
+    $chk =  "사용가능한 아이디 입니다.";
+} else if ($count == "1") {
+    $chk =  "중복된 아이디 입니다.";
 }
+$script = "<script>    function id_close(){
+    if({$count} == \"0\"){
+    window.close(\"board_id_chk.php\");
+    }
+    else if ({$count} == \"1\"){
+        
+        var parent = window.opener;
+        window.close(\"board_id_chk.php\");
+        parent.document.getElementById('user_id').value = '';
+        parent.document.getElementById('user_id').focus();
+    }
+}</script>"
 ?>
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h3 style="text-align:center"><?php //echo $chk ?></h3>
+    <!-- <br /><br /> -->
+    <div style="text-align:center">
+        <input type="button" onclick="id_close()" value="확인">
+    </div>
+</body>
+<?php echo $script?>
+</html>
