df098712f0b2aabc5a66281b5357052f6cb301c8
sieun park <89372116+singny@users.noreply.github.com>
2022-01-05 15:55:15 +0900

    Update user_view.php

--- 21일차/admin/user_view.php
+++ 21일차/admin/user_view.php
@@ -13,13 +13,12 @@ $user_sql = "SELECT user_id, user_name, dept_id, duty_id, loc_id
             FROM ex_user_set
             WHERE uno='{$post["uno"]}'";
 $db->query($user_sql);
-while ($db->next_record()) {
-  $row[3] = $db->Record;
-  $user_id = $row[3]["user_id"];
-  $user_name = $row[3]["user_name"];
+while($db->next_record()){
+    $row[3] = $db->Record;
+    $user_id = $row[3]["user_id"];
+    $user_name = $row[3]["user_name"];
 }
 
-$uno = $post["uno"];
 $dept_id = null;
 $sql = "SELECT * FROM {$_table_dept} WHERE LEVEL_NO=2 OR LEVEL_NO=3 OR LEVEL_NO=4";
 $opt_dept_id = null;
@@ -33,7 +32,7 @@ if ($db->nf() > 0) {
     $_sel = "";
 
     if ($db->f("dept_no") == $row[3]["dept_id"]) {
-      $_sel = " selected";
+        $_sel = " selected";
     }
 
     $txt = str_replace(" ", "&nbsp", $db->f("dept_name_lvl"));
@@ -50,7 +49,7 @@ if ($db->nf() > 0) {
     $_sel2 = "";
 
     if ($db->f("duty_no") == $row[3]["duty_id"]) {
-      $_sel2 = " selected";
+        $_sel2 = " selected";
     }
 
     $txt2 = str_replace(" ", "&nbsp", $db->f("duty_name"));
@@ -67,29 +66,12 @@ if ($db->nf() > 0) {
     $_sel3 = "";
 
     if ($db->f("loc_no") == $row[3]["loc_id"]) {
-      $_sel3 = " selected";
+        $_sel3 = " selected";
     }
 
     $txt3 = str_replace(" ", "&nbsp", $db->f("LOC_NAME"));
     $opt_loc_id .= "<option value=\"{$db->f("LOC_NO")}\" {$_sel3} title=\"{$db->f("LOC_NAME")}\">{$txt3}</option>\n";
   }
-}
-
-
-// 유저 사진
-$file_sql = "SELECT UNO, FILE_PATH FROM {$_table_user} WHERE UNO = {$uno}";
-$db->query($file_sql);
-while ($db->next_record()) {
-  $row[4] = $db->Record;
-}
-
-@$file_name = basename($row[4]["FILE_PATH"]);
-$user_img = null;
-
-if ($file_name) {
-  $user_img = "<img src=\"../upload_file/user/{$uno}/$file_name\" width=\"125px\" height=\"150px\"  alt=\"\">";
-} else {
-  $user_img = "<img src=\"../image/noimg.png\" id=\"image\" width=\"125px\" height=\"150px\" />";
 }
 ?>
 <!DOCTYPE html>
@@ -104,21 +86,21 @@ if ($file_name) {
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Jua&family=Nanum+Pen+Script&family=Poor+Story&display=swap" rel="stylesheet">
-  <link rel="apple-touch-icon" sizes="57x57" href="../image/icon/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="../image/icon/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="../image/icon/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="../image/icon/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="../image/icon/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="../image/icon/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="../image/icon/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="../image/icon/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="../image/icon/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="../image/icon/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="../image/icon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="../image/icon/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="../image/icon/favicon-16x16.png">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jua&family=Nanum+Pen+Script&family=Poor+Story&display=swap" rel="stylesheet">
+<link rel="apple-touch-icon" sizes="57x57" href="../image/icon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../image/icon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../image/icon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../image/icon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../image/icon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../image/icon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../image/icon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../image/icon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../image/icon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../image/icon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../image/icon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../image/icon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../image/icon/favicon-16x16.png">
   <style>
     body {
       min-height: 100vh;
@@ -129,11 +111,10 @@ if ($file_name) {
       background: -o-linear-gradient(bottom left, #eff6fd 0%, #296eaf 100%);
       background: linear-gradient(to top right, #eff6fd 0%, #296eaf 100%); */
       background-image: url('../image/join.png');
-      background-repeat: no-repeat;
-      background-size: cover;
+      background-repeat : no-repeat;
+      background-size:cover;
       font-family: 'Jua', sans-serif;
-
-
+      
     }
 
     .input-form {
@@ -151,29 +132,27 @@ if ($file_name) {
       -moz-box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.15);
       box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.15)
     }
-
     input[type=button] {
-      background-color: #6495ED;
-      color: white;
-      padding: 8px 20px;
-      border: none;
-      border-radius: 4px;
-      cursor: pointer;
-      width: 740px;
-      font-size: 20px;
-    }
+            background-color: #6495ED;
+            color: white;
+            padding: 8px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            width : 740px;
+            font-size: 20px;
+        }
 
     input[type=button]:hover {
-      background-color: #4682B4;
+            background-color: #4682B4;
 
     }
-
-    div[id=join_out] {
-      text-align: right;
-      font-size: 12pt;
-      color: rgb(164, 164, 164);
-      margin-left: 670px;
-      cursor: pointer;
+    div[id=join_out]{
+        text-align: right;
+        font-size: 12pt;
+        color: rgb(164, 164, 164);
+        margin-left: 670px;
+        cursor:pointer;
     }
   </style>
 </head>
@@ -183,48 +162,42 @@ if ($file_name) {
     <div class="input-form-backgroud row">
       <div class="input-form col-md-12 mx-auto">
         <h4 class="mb-3">회원 정보</h4>
-        <br />
-        <form class="validation-form" novalidate method="post" action="../board_user_modify.php?mode=user_modify" enctype="multipart/form-data">
-          <input type="hidden" id="uno" name="uno" value="<?php echo $uno ?>">
+        <form class="validation-form" novalidate method="post" action="user_modify.php?mode=modify&uno=<?php echo $post["uno"]?>">
           <div class="row">
             <div class="col-md-6 mb-3">
               <label for="user_id">아이디</label>
-              <input type="text" class="form-control" id="user_id" name="user_id" value="<?php echo $user_id ?>" disalbed readonly onfocus="this.blur()" required>
+              <input type="text" class="form-control" id="user_id" name="user_id" value="<?php echo $user_id?>" disalbed readonly onfocus="this.blur()" required>
               <div class="invalid-feedback">
                 아이디를 입력해주세요.
               </div>
-              <br />
-              <label for="user_name">이름</label>
-              <input type="text" class="form-control" id="user_name" name="user_name" value="<?php echo $user_name ?>" disabled required>
-              <div class="invalid-feedback">
-                이름을 입력해주세요.
-              </div>
             </div>
-            <div class="col-md-6 mb-3" style="text-align:right; padding-right:7%">
-              <label for="img">사진</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <!-- <img src="./image/noimg.png" id="image" width="125px" height="150px" /> -->
-              <?php echo $user_img ?>
-              <div id="change_img" hidden>
-                <input type="file" name="uploadfile" id="img" style="display:none;" accept="image/gif,image/jpeg,image/png" onchange="previewFile()" />
-                <label for="img" style="background-color:cadetblue;border-radius: 5px; margin-right:4%; margin-top:2px; padding:2px; cursor:pointer">이미지 변경하기</label>
-              </div>
-            </div>
+            <!-- <div class="col-md-6 mb-3">
+              <div><label for="user_id">&nbsp;</label></div>
+              <input type="button" id="id_check" value="아이디 중복확인" onclick="id_chk()"/>
+            </div> -->
           </div>
 
           <div class="row">
             <div class="col-md-6 mb-3">
-              <label for="user_pwd" id="la_user_pwd" hidden>비밀번호</label>
-              <input type="password" class="form-control" id="user_pwd" name="user_pwd" disabled value="" required hidden>
+              <label for="user_pwd" id="la_user_pwd" >비밀번호</label>
+              <input type="password" class="form-control" id="user_pwd" name="user_pwd"  value="" required >
               <div class="invalid-feedback">
                 비밀번호를 입력해주세요.
               </div>
             </div>
             <div class="col-md-6 mb-3">
-              <label for="user_pwd_chk" id="la_user_pwd_chk" hidden>비밀번호 확인</label>
-              <input type="password" class="form-control" id="user_pwd_chk" value="" disabled required hidden>
+              <label for="user_pwd_chk" id="la_user_pwd_chk" >비밀번호 확인</label>
+              <input type="password" class="form-control" id="user_pwd_chk" value=""  required >
               <div class="invalid-feedback">
                 비밀번호 확인을 입력해주세요.
               </div>
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="user_name">이름</label>
+            <input type="text" class="form-control" id="user_name" name="user_name" value="<?php echo $user_name?>"  required>
+            <div class="invalid-feedback">
+              이름을 입력해주세요.
             </div>
           </div>
           <div class="mb-3">
@@ -234,7 +207,7 @@ if ($file_name) {
           <div class="row">
             <div class="col-md-6 mb-3">
               <label for="dept_name">부서명</label>
-              <select class="custom-select d-block w-100" id="dept_id" name="dept_id" disabled required>
+              <select class="custom-select d-block w-100" id="dept_id" name="dept_id"  required>
                 <option value="" selected disalbed hidden></option>
                 <?php echo $opt_dept_id; ?>
               </select>
@@ -245,7 +218,7 @@ if ($file_name) {
             <div class="col-md-6 mb-3">
               <div class="mb-3">
                 <label for="duty_name">직급</label>
-                <select class="custom-select d-block w-100" id="duty_id" name="duty_id" disabled required>
+                <select class="custom-select d-block w-100" id="duty_id" name="duty_id"  required>
                   <option value="" selected disalbed hidden></option>
                   <?php echo $opt_duty_id ?>
                 </select>
@@ -258,7 +231,7 @@ if ($file_name) {
           <div class="row">
             <div class="col-md-8 mb-3">
               <label for="loc_name">회사 위치</label>
-              <select class="custom-select d-block w-100" id="loc_id" name="loc_id" disabled required>
+              <select class="custom-select d-block w-100" id="loc_id" name="loc_id"  required>
                 <option value="" selected disalbed hidden></option>
                 <?php echo $opt_loc_id ?>
               </select>
@@ -266,20 +239,20 @@ if ($file_name) {
                 회사 위치를 선택해주세요.
               </div>
             </div>
-          </div>
-          <div id="join_out">
+           </div>
+           <div id="join_out">
             <div onclick="really()">회원탈퇴</div>
-          </div>
+            </div>
 
           <hr class="mb-4">
           <div class="custom-control custom-checkbox" style="text-align:center">
             <!-- <input type="checkbox" class="custom-control-input" id="aggrement" name="aggrement" onclick="modifyMode()"> -->
-            <input type="checkbox" id="modify" name="modify" onclick="modifyMode()">
-            <label for="aggrement"><b>정보수정 mode</b></label>
+            <input type="checkbox" id="modify" name="modify" onclick="modifyMode()" checked>
+            <label for="aggrement"><b>수정 MODE</b></label>
           </div>
           <div class="mb-4"></div>
-          <input type="button" id="ok" name="ok" onclick="complete()" value="회원 목록" />
-          <button class="btn btn-primary btn-lg btn-block" id="modify_btn" name="modify_btn" type="submit" onclick="modifyComplete()" hidden>수정 완료</button>
+          <input type="button" id="ok" name="ok" onclick="complete()" value="회원목록" hidden/>
+          <button class="btn btn-primary btn-lg btn-block" id="modify_btn" name="modify_btn" type="submit" onclick="modifyComplete()" >수정 완료</button>
         </form>
       </div>
     </div>
@@ -294,88 +267,69 @@ if ($file_name) {
             event.preventDefault();
             event.stopPropagation();
           }
-          form.classList.add('was-validated');
+        form.classList.add('was-validated');
         }, false);
       });
     }, false);
 
-    function complete() {
-      location.href = "user_list.php";
+    function complete(){
+            location.href = "user_list.php";
+           }
+
+    function modifyComplete(){
+        var pwd = document.getElementById("user_pwd").value;
+            var pwd_chk = document.getElementById("user_pwd_chk").value;
+        
+            if(!(pwd === pwd_chk)){
+              alert("입력한 두 비밀번호가 일치하지 않습니다. 다시 입력해주세요.")
+              document.getElementById("user_pwd").value = '';
+              document.getElementById("user_pwd_chk").value = '';
+              document.getElementById("user_pwd").focus();
+              event.preventDefault();
+              event.stopPropagation();
+            }
     }
 
-    function modifyComplete() {
-      var pwd = document.getElementById("user_pwd").value;
-      var pwd_chk = document.getElementById("user_pwd_chk").value;
+    function modifyMode(){
+        var check = document.getElementById("modify").checked;
 
-      if (!(pwd === pwd_chk)) {
-        alert("입력한 두 비밀번호가 일치하지 않습니다. 다시 입력해주세요.")
-        document.getElementById("user_pwd").value = '';
-        document.getElementById("user_pwd_chk").value = '';
-        document.getElementById("user_pwd").focus();
-        event.preventDefault();
-        event.stopPropagation();
-      }
+        if(check){
+            document.getElementById("user_pwd").disabled = false;
+            document.getElementById("user_pwd_chk").disabled = false;
+            document.getElementById("user_name").disabled = false;
+            document.getElementById("dept_id").disabled = false;
+            document.getElementById("duty_id").disabled = false;
+            document.getElementById("loc_id").disabled = false;
+            document.getElementById("modify_btn").hidden = false;
+            document.getElementById("ok").hidden = true;
+            document.getElementById("la_user_pwd").hidden = false;
+            document.getElementById("user_pwd").hidden = false;
+            document.getElementById("la_user_pwd_chk").hidden = false;
+            document.getElementById("user_pwd_chk").hidden = false;
+            
+        }
+        else{
+            document.getElementById("user_pwd").disabled = true;
+            document.getElementById("user_pwd_chk").disabled = true;
+            document.getElementById("user_name").disabled = true;
+            document.getElementById("dept_id").disabled = true;
+            document.getElementById("duty_id").disabled = true;
+            document.getElementById("loc_id").disabled = true;
+            document.getElementById("ok").hidden = false;
+            document.getElementById("modify_btn").hidden = true;
+            document.getElementById("la_user_pwd").hidden = true;
+            document.getElementById("user_pwd").hidden = true;
+            document.getElementById("la_user_pwd_chk").hidden = true;
+            document.getElementById("user_pwd_chk").hidden = true;
+            
+        }
     }
 
-    function modifyMode() {
-      var check = document.getElementById("modify").checked;
-
-      if (check) {
-        document.getElementById("user_pwd").disabled = false;
-        document.getElementById("user_pwd_chk").disabled = false;
-        document.getElementById("user_name").disabled = false;
-        document.getElementById("dept_id").disabled = false;
-        document.getElementById("duty_id").disabled = false;
-        document.getElementById("loc_id").disabled = false;
-        document.getElementById("modify_btn").hidden = false;
-        document.getElementById("ok").hidden = true;
-        document.getElementById("la_user_pwd").hidden = false;
-        document.getElementById("user_pwd").hidden = false;
-        document.getElementById("la_user_pwd_chk").hidden = false;
-        document.getElementById("user_pwd_chk").hidden = false;
-        document.getElementById("change_img").hidden = false;
-
-      } else {
-        document.getElementById("user_pwd").disabled = true;
-        document.getElementById("user_pwd_chk").disabled = true;
-        document.getElementById("user_name").disabled = true;
-        document.getElementById("dept_id").disabled = true;
-        document.getElementById("duty_id").disabled = true;
-        document.getElementById("loc_id").disabled = true;
-        document.getElementById("ok").hidden = false;
-        document.getElementById("modify_btn").hidden = true;
-        document.getElementById("la_user_pwd").hidden = true;
-        document.getElementById("user_pwd").hidden = true;
-        document.getElementById("la_user_pwd_chk").hidden = true;
-        document.getElementById("user_pwd_chk").hidden = true;
-        document.getElementById("change_img").hidden = true;
-
-      }
-    }
-
-    function really() {
-
-      if (confirm("정말 탈퇴하시겠습니까?")) {
+    function really(){
+      
+      if(confirm("정말 탈퇴하시겠습니까?")){
         location.href = "board_user_modify.php?mode=delete";
       }
-    }
-
-
-    function previewFile() {
-            var preview = document.querySelector('img');
-            var file = document.querySelector('input[type=file]').files[0];
-            var reader = new FileReader();
-
-            reader.addEventListener("load", function() {
-                preview.src = reader.result;
-            }, false);
-
-            if (file) {
-                reader.readAsDataURL(file);
-                var reader_image = document.getElementById("image").style.visibility = "visible";
-            }
-            var attach = document.getElementById("attach").hidden = true;
-
     }
   </script>
 </body>
