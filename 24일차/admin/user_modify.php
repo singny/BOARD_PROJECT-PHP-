f27b0db3d71928e3482970a50dc1cab0d68147e6
sieun park <89372116+singny@users.noreply.github.com>
2022-01-07 18:04:35 +0900

    Create user_modify.php

---
+++ 24일차/admin/user_modify.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+@ini_set("display_errors", "On");
+//@error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+if(!defined("_INCLUDE_")) require_once $_SERVER["DOCUMENT_ROOT"] . "/lib/include.php";
+@error_reporting(E_ALL);
+
+include_once "_inc.php";
+
+$db = new DB;
+
+Fun::trimAll($post);
+
+if($post["mode"] == "modify"){
+
+        $db->BeginTransaction();
+        try{
+            $sql = "UPDATE {$_table_user} SET"
+                . "   USER_NAME = '{$post["user_name"]}'"
+                . " , USER_PWD = '{$post["user_pwd"]}'"
+                . " , DEPT_ID = '{$post["dept_id"]}'"
+                . " , DUTY_ID = '{$post["duty_id"]}'"
+                . " , LOC_ID = '{$post["loc_id"]}'"
+                . " WHERE uno = '{$post["uno"]}'";
+            $db->query($sql);
+            $db->commit();
+        } catch (Exception $ex) {
+            $db->rollback();
+            echo $ex;
+            exit;
+        }
+        $db->EndTransaction();
+        Fun::alert("회원정보를 정상적으로 수정하였습니다.", "user_view.php?uno={$post["uno"]}");
+}
+else if($post["mode"] == "delete"){
+    $db->BeginTransaction();
+    try{
+        $sql = "UPDATE {$_table_user} SET"
+            . " USER_PWD = null"
+            . " , IS_USE = 'N'"
+            . " , IS_ACTIVE = 'N'"
+            . " WHERE UNO = '{$post["uno"]}'";
+        $db->query($sql);
+        $db->commit();
+    } catch (Exception $ex) {
+        $db->rollback();
+        echo $ex;
+        exit;
+    }
+    $db->EndTransaction();
+    unset($_SESSION["user_id"]);
+    Fun::alert("탈퇴가 완료되었습니다.", "user_list.php");
+}
+?>
