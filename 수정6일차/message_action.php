9e04be1ad3bb38fc241616a18f8c675afaee77c2
sieun park <89372116+singny@users.noreply.github.com>
2022-01-20 17:31:34 +0900

    Create message_action.php

---
+++ 수정6일차/message_action.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+@ini_set("display_errors", "On");
+//@error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+if (!defined("_INCLUDE_")) require_once $_SERVER["DOCUMENT_ROOT"] . "/lib/include.php";
+@error_reporting(E_ALL);
+
+include_once "_inc.php";
+
+$db = new DB;
+
+$m_title = htmlspecialchars(Fun::convVal2DB($post["m_title"]));
+$m_body = htmlspecialchars(Fun::convVal2DB($post["m_body"]));
+
+$db->BeginTransaction();
+try {
+    $mno = $db->nextid("SEQ_" . $_table_message);
+    $sql = "INSERT INTO {$_table_message} ("
+        . " MNO, WR_USER, RE_USER, M_DATETIME, M_TITLE, M_BODY"
+        . ") VALUES ("
+        . " {$mno},{$post["wr_user"]},{$post["re_user"]}, sysdate,'{$m_title}','{$m_body}'"
+        . ")";
+    $db->query($sql);
+    $db->commit();
+} catch (Exception $ex) {
+    $db->rollback();
+    echo $ex;
+    exit;
+}
+$db->EndTransaction();
+Fun::alert("쪽지가 정상적으로 보내졌습니다.","send_list.php")
+?>
